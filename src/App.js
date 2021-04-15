3e4c04d807aff58e362cd6c7ab45406fa0c37efa
Nikunj Handa <nikunj.sg@gmail.com>
2021-04-14 20:12:02 -0700

    typo

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ function App() {
       <header className="App-header">
         <h1>Nikunj Handa</h1>
         <p>
-          👋 I'm Nikunj Handa. I live near San Francisco and grew up in Singapore, Ghana, Oman, and India. I used worked at <a href="https://stripe.com">Stripe</a> and <a href="https://meltwater.org">MEST Africa</a>.
+          👋 I'm Nikunj Handa. I live near San Francisco and grew up in Singapore, Ghana, Oman, and India. I used to work at <a href="https://stripe.com">Stripe</a> and <a href="https://meltwater.org">MEST Africa</a>.
         </p>
         <p>
-          Here are some ways you can get in touch:
           <a href="mailto:nikunj.sg@gmail.com"> nikunj.sg@gmail.com</a>
            &nbsp;|&nbsp;
           <a href="http://twitter.com/nikunjhanda">Twitter</a>
